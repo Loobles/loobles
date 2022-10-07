6380eb297adf26e9030c59dfbb2f8969e3385dbb
Eric Afes <eriicafes@gmail.com>
2022-10-07 09:44:20 +0100

    Replace absolute positioning in navbar with grid

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -3,19 +3,18 @@ import Link from "next/link"
 
 export function Navbar() {
   return (
-    <header className="relative flex items-center justify-between px-content py-2 ">
+    <header className="grid grid-cols-3 items-center px-content py-2">
       <Link href="/">
-        <a className="text-xl font-semibold">Loobles</a>
+        <a className="text-xl font-semibold mr-auto">Loobles</a>
       </Link>
 
-      <nav className="absolute -z-10 inset-0 flex items-center justify-center">
-        <Link href="/">
+      <nav className="flex items-center justify-center">
+        <Link href="/loobtoons">
           <a className="font-light">Projects</a>
         </Link>
       </nav>
 
-
-      <button className="p-2">
+      <button className="p-2 ml-auto">
         <Bars3Icon className="w-7 h-7" />
       </button>
     </header>
